b20bd9e29e08c721fedda897889963a5ee7212ae
sagarsalyan <sagarslyn@gmail.com>
2020-03-02 16:34:35 +0530

    NodeJs Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fileuploadangularnode';
+
+  uploadedFiles: Array < File > ;
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    ngOnInit() {
+
+    }
+
+
+  fileChange(element) {
+        this.uploadedFiles = element.target.files;
+  }
+
+  upload() {
+    let formData = new FormData();
+    for (var i = 0; i < this.uploadedFiles.length; i++) {
+        formData.append("uploads[]", this.uploadedFiles[i], this.uploadedFiles[i].name);
+    }
+    this.http.post('/api/upload', formData)
+    .subscribe((response) => {
+         console.log('response received is ', response);
+    })
+  }
+
 }
